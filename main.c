ff6b9cecdf449cd403db9498878107d1020afaa3
joycegichuki <joycegichuki31@gmail.com>
2026-09-22 22:22:57 +0900

    Simplify main function to print 'Hello, World!'
    Replaced arithmetic operations with a simple greeting.

--- main.c
+++ main.c
@@ -1,13 +1,7 @@
 #include <stdio.h>
-int main () 
+int main() 
 {
-    int x=20;
-    int y=10;
-
-    printf ("Sum of the two numbers: %d\n", x+y);
-    printf ("Difference of the two numbers: %d\n", x-y);
-    printf ("Product of the two numbers: %d\n", x*y);
-    printf ("Quotient of the two numbers: %d\n", x/y);
-    
+   
+     printf ("Hello, World!");
     return 0;
 }
